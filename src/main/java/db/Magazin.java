8d4e73d11513700b76d7edbae6b37d283654e7ab
Alexandru Topala <alexandru.top98@gmail.com>
2020-03-20 21:55:28 +0200

    initial commit

---
+++ src/main/java/db/Magazin.java
@@ -0,0 +1,42 @@
+package db;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class Magazin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nume;
+
+    @ElementCollection
+    @CollectionTable(name = "MY_COLLECTION_TABLE")
+    private Collection<String> numere;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public Collection<String> getNumere() {
+        return numere;
+    }
+
+    public void setNumere(Collection<String> numere) {
+        this.numere = numere;
+    }
+}
