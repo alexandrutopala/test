1da99b53327ebebd7f45959d087fd60956ffaf4a
Alexandru Topala <alexandru.top98@gmail.com>
2020-03-20 22:19:33 +0200

    alex commit

--- src/main/java/main/Exemplu7.java
+++ src/main/java/main/Exemplu7.java
@@ -2,6 +2,7 @@ package main;
 
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 
+// comentariul meu
 public class Exemplu7 {
     public static void main(MysqlxDatatypes.Scalar.String[] args) {
         System.out.println("Hello from Bogdan");
